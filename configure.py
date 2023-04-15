4a43bf8af9b82ce40c83836e5336895bbe724ad7
luoyin <912271673@qq.com>
2023-04-15 13:21:56 +0800

    update

--- configure.py
+++ configure.py
@@ -25,6 +25,7 @@ commonflags = "-O3 -ffreestanding"
 
 cflags = commonflags
 cxxflags = commonflags
+rustflags = commonflags
 
 all_build_tools = [cc,cxx,ld,make,rustc]
 
@@ -64,15 +65,22 @@ makefile_contains = str()
 
 def gen_makefile(_source_path,_build_dir):
     os.chdir(_source_path)
+    print("entering " + _source_path)
 
     #add _build_dir into pre_create_dirs
     add_pre_create_dir(_build_dir)
+
+    #backup the flags
+    bak_cflags = cflags
+    bak_cxxflags = cxxflags
+    bak_rustflags = rustflags
 
     file_list = listdir()
     dir_list = list()
     configuration_json = dict()
     c_cxx_includes = list()
     if "configuration.json" in file_list:#read the configuration.json
+        print("configuration.json found")
         file_list.remove("configuration.json")
         configuration_json = json.loads(Path("configuration.json").read_text())
         if "ignore" in list(configuration_json.keys()):#ignore files and directories
@@ -80,7 +88,7 @@ def gen_makefile(_source_path,_build_dir):
             if not isinstance(ignore_file_dir_list,list):
                 print("in " + path.abspath("configuration.json") + " \"ignore\" should be an array")
                 print("exit")
-                exit()
+                exit(1)
             for ignore_file_dir in ignore_file_dir_list:
                 if ignore_file_dir in file_list:
                     file_list.remove(ignore_file_dir)
@@ -91,8 +99,26 @@ def gen_makefile(_source_path,_build_dir):
             if not isinstance(includes__,list):
                 print("in " + path.abspath("configuration.json") + " \"c_cxx_includes\" should be an array")
                 print("exit")
-                exit()
+                exit(1)
             c_cxx_includes = includes__
+        if "cflags" in list(configuration_json.keys()):
+            if not isinstance(configuration_json["cflags"],str):
+                print("in " + path.abspath("configuration.json") + " \"cflags\" should be a string")
+                print("exit")
+                exit(1)
+            cflags = configuration_json["cflags"]
+        if "cxxflags" in list(configuration_json.keys()):
+            if not isinstance(configuration_json["cxxflags"],str):
+                print("in " + path.abspath("configuration.json") + " \"cxxflags\" should be a string")
+                print("exit")
+                exit(1)
+            cxxflags = configuration_json["cxxflags"]
+        if "rustflags" in list(configuration_json.keys()):
+            if not isinstance(configuration_json["rustflags"],str):
+                print("in " + path.abspath("configuration.json") + " \"rustflags\" should be a string")
+                print("exit")
+                exit(1)
+            rustflags = configuration_json["rustflags"]
     file_list_back = file_list
     for file_dir_name__ in file_list_back:
         if path.isdir(file_dir_name__):
@@ -101,27 +127,44 @@ def gen_makefile(_source_path,_build_dir):
     for source_file in file_list:#add header files into c_cxx_includes
         if path.splitext(source_file)[-1] == ".h":
             c_cxx_includes.append(source_file)
+            print(source_file)
         if path.splitext(source_file)[-1] == ".hxx":
             c_cxx_includes.append(source_file)
+            print(source_file)
         if path.splitext(source_file)[-1] == ".hpp":
             c_cxx_includes.append(source_file)
+            print(source_file)
         if path.splitext(source_file)[-1] == ".hh":
             c_cxx_includes.append(source_file)
+            print(source_file)
     for source_file in file_list:#gen makefile
         if path.splitext(source_file)[-1] == ".c":
-            c_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + ".o", c_cxx_includes)
+            c_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + "_c.o", c_cxx_includes,cflags)
+            print(source_file + " : " + path.splitext(source_file)[0] + "_c.o")
         if path.splitext(source_file)[-1] == ".cxx":
-            cxx_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + ".o", c_cxx_includes)
+            cxx_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + "_cxx.o", c_cxx_includes,cxxflags)
+            print(source_file + " : " + path.splitext(source_file)[0] + "_cxx.o")
         if path.splitext(source_file)[-1] == ".cc":
-            cxx_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + ".o", c_cxx_includes)
+            cxx_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + "_cc.o", c_cxx_includes,cxxflags)
+            print(source_file + " : " + path.splitext(source_file)[0] + "_cc.o")
         if path.splitext(source_file)[-1] == ".cpp":
-            cxx_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + ".o", c_cxx_includes)
+            cxx_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + "_cpp.o", c_cxx_includes,cxxflags)
+            print(source_file + " : " + path.splitext(source_file)[0] + "_cpp.o")
         if path.splitext(source_file)[-1] == ".s":
-            asm_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + ".o", c_cxx_includes)
+            asm_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + "_s.o", c_cxx_includes,cflags)
+            print(source_file + " : " + path.splitext(source_file)[0] + "_s.o")
         if path.splitext(source_file)[-1] == ".asm":
-            asm_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + ".o", c_cxx_includes)
+            asm_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + "_asm.o", c_cxx_includes,cflags)
+            print(source_file + " : " + path.splitext(source_file)[0] + "_asm.o")
         if path.splitext(source_file)[-1] == ".S":
-            asm_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + ".o", c_cxx_includes)
+            asm_file_com(path.abspath(source_file),path.abspath(_build_dir) + "/" + path.splitext(source_file)[0] + "_S.o", c_cxx_includes,cflags)
+            print(source_file + " : " + path.splitext(source_file)[0] + "_S.o")
+
+    #restore flags
+    cflags = bak_cflags
+    cxxflags = bak_cxxflags
+    rustflags = bak_rustflags
+
     for source_dir in dir_list:
         sp = _source_path
         bd = _build_dir
@@ -131,7 +174,7 @@ def gen_makefile(_source_path,_build_dir):
 def add_line(_T:str):
     makefile_contains = makefile_contains + _T + "\n"
 
-def c_file_com(_I:str,_O:str,_R:list):#gen c language makefile
+def c_file_com(_I:str,_O:str,_R:list,flags:str):#gen c language makefile
     relays = str()
     relays_dir_list = list()
     relays_file_list = list()
@@ -140,7 +183,7 @@ def c_file_com(_I:str,_O:str,_R:list):#gen c language makefile
             print(path.abspath(relay_file) + " doesn't exist")
             print("but " + _I + " needs it")
             print("exit")
-            exit()
+            exit(1)
         if path.isdir(relay_file):
             if not path.abspath(relay_file) in relays_dir_list:
                 relays_dir_list.append(path.abspath(relay_file))
@@ -160,9 +203,9 @@ def c_file_com(_I:str,_O:str,_R:list):#gen c language makefile
     for includes in relays_file_list:
         relays = relays + " " + includes
     add_line(_O + ":" + relays + " " + _I)
-    add_line("\t" + cc + " " + cc_includes + " " + cflags + " " + _I + " -o " + _O)
+    add_line("\t" + cc + " " + cc_includes + " " + flags + " -c " + _I + " -o " + _O)
 
-def cxx_file_com(_I:str,_O:str,_R:list):#gen c++ language makefile
+def cxx_file_com(_I:str,_O:str,_R:list,flags:str):#gen c++ language makefile
     relays = str()
     relays_dir_list = list()
     relays_file_list = list()
@@ -171,7 +214,7 @@ def cxx_file_com(_I:str,_O:str,_R:list):#gen c++ language makefile
             print(path.abspath(relay_file) + " doesn't exist")
             print("but " + _I + " needs it")
             print("exit")
-            exit()
+            exit(1)
         if path.isdir(relay_file):
             if not path.abspath(relay_file) in relays_dir_list:
                 relays_dir_list.append(path.abspath(relay_file))
@@ -191,9 +234,9 @@ def cxx_file_com(_I:str,_O:str,_R:list):#gen c++ language makefile
     for includes in relays_file_list:
         relays = relays + " " + includes
     add_line(_O + ":" + relays + " " + _I)
-    add_line("\t" + cxx + " " + cxx_includes + " " + cxxflags + " " + _I + " -o " + _O)
+    add_line("\t" + cxx + " " + cxx_includes + " " + flags + " -c " + _I + " -o " + _O)
 
-def asm_file_com(_I:str,_O:str,_R:list):#gen c language makefile
+def asm_file_com(_I:str,_O:str,_R:list,flags:str):#gen c language makefile
     relays = str()
     relays_dir_list = list()
     relays_file_list = list()
@@ -202,7 +245,7 @@ def asm_file_com(_I:str,_O:str,_R:list):#gen c language makefile
             print(path.abspath(relay_file) + " doesn't exist")
             print("but " + _I + " needs it")
             print("exit")
-            exit()
+            exit(1)
         if path.isdir(relay_file):
             if not path.abspath(relay_file) in relays_dir_list:
                 relays_dir_list.append(path.abspath(relay_file))
@@ -222,7 +265,7 @@ def asm_file_com(_I:str,_O:str,_R:list):#gen c language makefile
     for includes in relays_file_list:
         relays = relays + " " + includes
     add_line(_O + ":" + relays + " " + _I)
-    add_line("\t" + cc + " " + cc_includes + " " + cflags + " -c " + _I + " -o " + _O)
+    add_line("\t" + cc + " " + cc_includes + " " + flags + " -c " + _I + " -o " + _O)
 
 #gen makefile function end
 
@@ -235,8 +278,12 @@ project_source_dir = path.abspath(path.dirname(__file__))
 
 if __name__ == "__main__":
     check_tools(all_build_tools)
-    gen_makefile(project_source_dir, project_build_dir)
+    gen_makefile(project_source_dir, project_build_dir + "/build")
     dir_create(pre_create_dirs_abs_)
-
+    makefile_output = open(project_build_dir + "/Makefile","w")
+    makefile_output.writelines(makefile_contains)
+    makefile_output.close()
+    print("done")
+    exit(0)
 
 #main function end
