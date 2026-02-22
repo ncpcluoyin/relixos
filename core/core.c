37f3121f00d8000439311c36df6f483867477e9e
ncpcluoyin <912271673@qq.com>
2026-02-22 10:48:29 +0800

    update

--- core/core.c
+++ core/core.c
@@ -2,4 +2,8 @@
 #include"serial.h"
 
 void core_main(){
+    serial_putchar(DEFAULT_COM,'A');
+    char a[3] = {'M','A','\0'};
+    serial_puts(DEFAULT_COM,a);
+    serial_putchar(DEFAULT_COM,'A');
 }
--- headers/serial.h
+++ headers/serial.h
@@ -3,19 +3,11 @@
 
 #include <stdint.h>
 
-// 初始化串口（默认 COM1，可修改端口）
-void serial_init(uint16_t port);
+#define DEFAULT_COM COM1
+#define COM1 0x3F8//unit16_t
+#define COM2 0x2F8
 
-// 输出一个字符
-void serial_putchar(char c);
-
-// 输出字符串
-void serial_puts(const char *str);
-
-// 输入一个字符（阻塞，直到收到字符）
-char serial_getchar(void);
-
-// 检查是否有字符可读（非阻塞）
-int serial_data_available(void);
+void serial_putchar(uint16_t port, uint8_t data);
+void serial_puts(uint16_t port,const char * str);
 
 #endif
